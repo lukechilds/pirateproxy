b856fa42da461d6a981d80c7da176f11a73678f5
Luke Childs <lukechilds123@gmail.com>
2017-05-29 00:41:09 +0700

    Replace urls in text content

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 const http = require('http');
 const got = require('got');
+const replaceStream = require('replacestream');
 
 const target = 'thepiratebay.org';
 
 http.createServer((req, res) => {
-	const copyData = origRes => {
+	let stream = got.stream(target + req.url);
+
+	const handleResponse = origRes => {
 		res.statusCode = origRes.statusCode;
 		Object.entries(origRes.headers).forEach(header => {
 			if (header[0] !== 'content-encoding') {
 				res.setHeader(header[0], header[1]);
 			}
 		});
+
+		if (origRes.headers['content-type'].includes('text')) {
+			stream = stream.pipe(replaceStream(target, req.headers.host));
+		}
+
+		stream.pipe(res);
 	};
 
-	got.stream(target + req.url)
-    .on('response', copyData)
-    .on('error', copyData)
-    .pipe(res);
+	stream
+    .on('response', handleResponse)
+    .on('error', handleResponse);
 }).listen(3000);
